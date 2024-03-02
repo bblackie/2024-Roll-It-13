e4e78c76bbe8d91f48ff1ce1db9aff30d25b1324
Brian James Blackie <122497534+bblackie@users.noreply.github.com>
2024-03-02 16:17:32 +1300

    Initial commit

---
+++ c_01_instructions.py
@@ -0,0 +1,15 @@
+
+print("🎲🎲 Roll it 13 🎲🎲")
+
+while True:
+    want_instructions = input("Do you want to read the instructions? ").lower()
+
+    if want_instructions == "yes" or want_instructions == "y":
+        print("You chose yes")
+    elif want_instructions == "no" or want_instructions == "n":
+        print("You chose no")
+    else:
+        print("You did not choose a valid response")
+
+
+
